6f8e3290c8efbc2112e12f2063bf22920c0c3403
prerna1234-git <prernasinghps23@gmail.com>
2026-04-27 17:05:15 +0530

    updated db config

--- finance-tracker-backend/src/config/db.js
+++ finance-tracker-backend/src/config/db.js
@@ -2,9 +2,8 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "finance_db",
-  password: "jbmarch1994",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
